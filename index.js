2ce4787d97df6bf2f1a0d1bb921bf8a43020e920
Natan Ramalho <natan.rsd13@gmail.com>
2026-01-17 22:16:30 -0300

    .

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.listen(3000, () => {
-  console.log(`Ouvindo porta 3000`)
-})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express()
+
+app.listen(3000, () => {
+  console.log(`Ouvindo porta 3000`)
+})
